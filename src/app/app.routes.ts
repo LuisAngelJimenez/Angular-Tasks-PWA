c628db31724d18550d2657ab80ab443f469c2bdf
Luis Jimenez <luisangelastellanos003@gmail.com>
2024-09-30 11:17:23 -0600

    add(manifest) adding manifest file and config json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'todo',
-    children:[
-    ]
-  },
+ 
   {
     path:'taskList',
             loadComponent: () => import('./features/TaskList/TaskList.component')
-        
   },
   {
     path: '**',
-    redirectTo: 'todo',
+    redirectTo: 'taskList',
   },
 ];
