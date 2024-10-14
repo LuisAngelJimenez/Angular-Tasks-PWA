72664b6ebddb8f0273303677ba795ccfacdb2d4e
Luis Jimenez <luisangelastellanos003@gmail.com>
2024-10-14 10:49:22 -0600

    feat(BeforeInstallPrompt) first try of BIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import NavbarComponent from "./components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PWA-AngularTasks';
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  OnBeforeInstallPrompt(event:Event){
+    event.preventDefault();
+    this.deferredPrompt=event;
+    this.showInstallButton=true;
+  }
+
+  InstalarPWA(){
+    if(this.deferredPrompt){
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice().then((choiceResult:any)=>{
+        if(choiceResult=='accepted'){
+          console.log('el usuario instalo el prompt');
+        }else{
+          console.log('no instalo nada el ue');
+        }
+        this.deferredPrompt=null;
+      })
+    }
+  }
 }
